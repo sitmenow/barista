237b6692955fd8b49bf5ae2981df585c445d8996
Gerardo Reyes <grevych@gmail.com>
2019-05-11 16:46:55 -0500

    Update actions for turns component:  - Add function to handle turn preparation  - Add function to handle turn unpreparation

--- src/components/turns/actions.js
+++ src/components/turns/actions.js
@@ -7,8 +7,13 @@ export const loadTurns = () =>
     const { brand, branch, barista } = getState();
 
     const api = new API().getInstance();
-    const brandResource = api.brand({ _id: brand.id });
-    const branchResource = brandResource.branch({ _id: branch.id });
+    const brandResource = api.brand({
+      _id: brand.id,
+    });
+    const branchResource = brandResource.branch({
+      _id: branch.id,
+      _brand: brandResource,
+    });
 
     dispatch({ type: globalActions.START_LOAD });
 
@@ -28,8 +33,8 @@ export const serveTurn = (turnId) =>
 
     const api = new API().getInstance();
     const brandResource = api.brand({ _id: brand.id });
-    const branchResource = brandResource.branch({ _id: branch.id });
-    const turnResource = branchResource.turn({ _id: turnId });
+    const branchResource = brandResource.branch({ _id: branch.id, _brand: brandResource });
+    const turnResource = branchResource.turn({ _id: turnId, _branch: branchResource });
 
     dispatch({ type: globalActions.START_LOAD });
 
@@ -52,8 +57,8 @@ export const rejectTurn = (turnId) =>
 
     const api = new API().getInstance();
     const brandResource = api.brand({ _id: brand.id });
-    const branchResource = brandResource.branch({ _id: branch.id });
-    const turnResource = branchResource.turn({ _id: turnId });
+    const branchResource = brandResource.branch({ _id: branch.id, _brand: brandResource });
+    const turnResource = branchResource.turn({ _id: turnId, _branch: branchResource });
 
     dispatch({ type: globalActions.START_LOAD });
 
@@ -70,15 +75,53 @@ export const rejectTurn = (turnId) =>
     dispatch({ type: globalActions.END_LOAD });
   }
 
-export const prepareTurn = () =>
-  async (dispatch: function, getstate: function) => {
-    dispatch({ type: globalActions.LOCK_BARISTA });
+export const prepareTurn = (turnId) =>
+  async (dispatch: function, getState: function) => {
     // LOCK BARISTA && UPDATE_TURN (status: preparing)
+
+    const { brand, branch, barista } = getState();
+
+    const api = new API().getInstance();
+    const brandResource = api.brand({ _id: brand.id });
+    const branchResource = brandResource.branch({ _id: branch.id, _brand: brandResource });
+    const turnResource = branchResource.turn({ _id: turnId, _branch: branchResource });
+
+    dispatch({ type: globalActions.START_LOAD });
+
+    try {
+      turnResource.prepare();
+      // dispatch({ type: actions.UPDATE_TURN /* , updatedTurn */ });
+      dispatch({ type: globalActions.LOCK_BARISTA });
+      // TODO: Show message of success
+    } catch(error) {
+      console.log('error preparing turn');
+    }
+
+    dispatch({ type: globalActions.END_LOAD });
   }
 
-export const unprepareTurn = () =>
-  async (dispatch: function, getstate: function) => {
-    dispatch({ type: globalActions.UNLOCK_BARISTA });
+export const unprepareTurn = (turnId) =>
+  async (dispatch: function, getState: function) => {
     // UNLOCK BARISTA && UPDATE_TURN (status: waiting)
+
+    const { brand, branch, barista } = getState();
+
+    const api = new API().getInstance();
+    const brandResource = api.brand({ _id: brand.id });
+    const branchResource = brandResource.branch({ _id: branch.id, _brand: brandResource });
+    const turnResource = branchResource.turn({ _id: turnId, _branch: branchResource });
+
+    dispatch({ type: globalActions.START_LOAD });
+
+    try {
+      turnResource.unprepare();
+      // dispatch({ type: actions.UPDATE_TURN /* , updatedTurn */ });
+      dispatch({ type: globalActions.UNLOCK_BARISTA });
+      // TODO: Show message of success
+    } catch(error) {
+      console.log('error preparing turn');
+    }
+
+    dispatch({ type: globalActions.END_LOAD });
   }
 
